01bebbeaccf52711a1b0d8ed9ce7a0de5596b061
mohs-in <in.mohsin@outlook.com>
2025-03-10 23:47:15 +0530

    optimalOne lc:34

--- src/binarySearch/FirstLastPosition.java
+++ src/binarySearch/FirstLastPosition.java
@@ -15,9 +15,50 @@ public class FirstLastPosition {
         return result;
     }
 
+    private static int lowerBound(int [] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        int res = n;
+        while(low <= high) {
+            int mid = (low+high)/2;
+            if(nums[mid] >= target) {
+                res = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return res;
+    }
+
+    private static int upperBound(int [] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        int res = n;
+        while(low <= high) {
+            int mid = (low+high)/2;
+            if(nums[mid] > target) {
+                res = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return res;
+    }
+
+    public static int[] optimalOne(int[] nums, int target) {
+        int lowerBound = lowerBound(nums, target);
+        int upperBound = upperBound(nums, target)-1;
+        if(lowerBound == nums.length || nums[lowerBound] != target)
+            return new int []{-1, -1};
+
+        else return new int []{lowerBound, upperBound};
+    }
+
     public static void main(String ...args) {
         int [] nums = {5,7,7,8,8,10};
         int target = 8;
-        System.out.println(Arrays.toString(brute(nums, target)));
+        System.out.println(Arrays.toString(optimalOne(nums, target)));
     }
 }
