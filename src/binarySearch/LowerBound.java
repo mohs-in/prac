0e4a09f2d5d713a3a5045dd50517d28c64ac3fe5
mohs-in <in.mohsin@outlook.com>
2025-03-10 23:52:32 +0530

    lower bound practice

---
+++ src/binarySearch/LowerBound.java
@@ -0,0 +1,27 @@
+package binarySearch;
+
+import java.util.Arrays;
+
+public class LowerBound {
+    private static int lowerBound(int [] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n - 1;
+        int res = n;
+        while(low <= high) {
+            int mid = (low+high)/2;
+            if(nums[mid] >= target) {
+                res = mid;
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return res;
+    }
+
+    public static void main(String ...args) {
+        int [] nums = {5,7,7,8,8,10};
+        int target = 8;
+        System.out.println(lowerBound(nums, target));
+    }
+}
