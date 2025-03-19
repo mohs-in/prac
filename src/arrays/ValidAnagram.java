a830cf1efc777566e154581fa3dc3ac2cb7bad19
mohs-in <in.mohsin@outlook.com>
2025-03-19 19:37:24 +0530

    optimal lc:242

--- src/arrays/ValidAnagram.java
+++ src/arrays/ValidAnagram.java
@@ -11,8 +11,21 @@ public class ValidAnagram {
         Arrays.sort(ta);
         return Arrays.equals(sa,ta);
     }
+    public static boolean isAnagramOptimal(String s, String t) {
+        int[] count = new int[26];
+
+        for (char c : s.toCharArray()) count[c - 'a']++;
+
+        for (char c : t.toCharArray()) count[c - 'a']--;
+
+        for (int i : count) {
+            if (i != 0) return false;
+        }
+
+        return true;
+    }
 
     public static void main(String ...args) {
-        System.out.println(isAnagramBrute("rat", "car"));
+        System.out.println(isAnagramOptimal("anna", "nana"));
     }
 }
